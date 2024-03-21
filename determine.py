f52eefe98fb14c451338795dda37c10cf5abea6c
AndreMercado <137970518+AndreMercado@users.noreply.github.com>
2024-03-20 21:25:05 -0500

    Update determine.py

--- determine.py
+++ determine.py
@@ -3,23 +3,29 @@ import json
 
 class determine:
     def __init__(self, verb, json_file):
+        self.json_file = json_file
 
         self.base = verb[:-1]
         self.v_end = verb[-1]
-        self.file = json.load(open(json_file, encoding='utf-8'))
+        self.file = json.load(open(self.json_file, encoding='utf-8'))
 
         if json_file == 'exception_suffixes.json':
             self.v_end = verb
             self.base = ""
 
     def stem(self, formality=None, energy=None):
-        return self.base
+        try:
+            if self.json_file == 'exception_suffixes.json':
+                return self.base + self.file['base'][self.v_end]
+            return self.base
+        except KeyError:
+            return 'None'
 
     def te_form(self, formality=None, energy=None):
         try:
             return self.base + self.file['te_form'][self.v_end]
         except KeyError:
-            return None
+            return 'None'
 
     def infinitive(self, formality=None, energy=None):
         try:
@@ -27,55 +33,55 @@ class determine:
                 return self.base
             return self.base + self.file['infinitive'][self.v_end]
         except KeyError:
-            return None
+            return 'None'
 
     def present(self, formality='plain', energy='positive'):
         try:
             return self.base + self.file['present'][formality][energy][self.v_end]
         except KeyError:
-            return None
+            return 'None'
 
     def past(self, formality='plain', energy='positive'):
         try:
             return self.base + self.file['past'][formality][energy][self.v_end]
         except KeyError:
-            return None
+            return 'None'
 
     def present_progressive(self, formality='plain', energy='positive'):
         try:
             return self.base + self.file['present_progressive'][formality][energy][self.v_end]
         except KeyError:
-            return None
+            return 'None'
 
     def past_progressive(self, formality='plain', energy='positive'):
         try:
             return self.base + self.file['past_progressive'][formality][energy][self.v_end]
         except KeyError:
-            return None
+            return 'None'
 
     def potential(self, formality='plain', energy='positive'):
         try:
             return self.base + self.file['potential'][formality][energy][self.v_end]
         except KeyError:
-            return None
+            return 'None'
 
     def causative(self, formality='plain', energy='positive'):
         try:
             return self.base + self.file['causative'][formality][energy][self.v_end]
         except KeyError:
-            return None
+            return 'None'
 
     def passive(self, formality='plain', energy='positive'):
         try:
             return self.base + self.file['passive'][formality][energy][self.v_end]
         except KeyError:
-            return None
+            return 'None'
 
     def causative_passive(self, formality='plain', energy='positive'):
         try:
             return self.base + self.file['causative_passive'][formality][energy][self.v_end]
         except KeyError:
-            return None
+            return 'None'
 
     def conditional_eba(self, formality='plain', energy='positive'):
         try:
@@ -83,21 +89,21 @@ class determine:
                 return 'n/a'
             return self.base + self.file['conditional_eba'][formality][energy][self.v_end]
         except KeyError:
-            return None
+            return 'None'
 
     def conditional_tara(self, formality='plain', energy='positive'):
         if self.past(formality, energy) is None:
-            return None
+            return 'None'
         return self.past(formality, energy) + "ら"
 
     def imperative(self, formality='plain', energy='positive'):
         try:
             return self.base + self.file['imperative'][formality][energy][self.v_end]
         except KeyError:
-            return None
+            return 'None'
 
     def volitional(self, formality='plain', energy='positive'):
         try:
             return self.base + self.file['volitional'][formality][energy][self.v_end]
         except KeyError:
-            return None
+            return 'None'
